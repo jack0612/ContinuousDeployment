4e10ce53d785a5ba9a92b9e642acf7a846fa1d1d
jacklu <jacklu0612@hotmail.com>
2018-10-22 06:45:24 +0800

    add unit tests

--- ContinuousWeb/Controllers/HomeController.cs
+++ ContinuousWeb/Controllers/HomeController.cs
@@ -14,13 +14,21 @@ namespace ContinuousWeb.Controllers
         {
             return View();
         }
-
+/* 
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
+        */
+        public IActionResult About()
+{
+    throw new NotImplementedException();
+
+    //ViewData["Message"] = "Your application description page.";
+    //return View();
+}
 
         public IActionResult Contact()
         {
---
+++ Tests/ContinuousWeb.Tests/UnitTest1.cs
@@ -0,0 +1,31 @@
+/* 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContinuousWeb.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
+*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContinuousWeb.Controllers;
+
+namespace ContinuousWeb.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {        
+        [TestMethod]
+        public void AboutPageTest()
+        {
+            var homeController = new HomeController();
+            var result = homeController.About();
+        }
+    }
+}
